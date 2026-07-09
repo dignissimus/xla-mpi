e2781510ed3278338ad6c9ec5393e26acb98b52e
Sam Ezeh <sam.z.ezeh@gmail.com>
2026-07-09 06:27:40 +0100

    Add custom paths for llvm and mlir

--- build.sh
+++ build.sh
@@ -15,14 +15,20 @@ mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
 
 echo "Configuring project with CMake..."
-STABLEHLO_FLAG=""
+CUSTOM_PATHS=""
 if [ -n "$STABLEHLO_DIR" ]; then
-    STABLEHLO_FLAG="-DStablehlo_DIR=$STABLEHLO_DIR"
+    CUSTOM_PATHS="$CUSTOM_PATHS -DStablehlo_DIR=$STABLEHLO_DIR"
+fi
+if [ -n "$MLIR_DIR" ]; then
+    CUSTOM_PATHS="$CUSTOM_PATHS -DMLIR_DIR=$MLIR_DIR"
+fi
+if [ -n "$LLVM_DIR" ]; then
+    CUSTOM_PATHS="$CUSTOM_PATHS -DLLVM_DIR=$LLVM_DIR"
 fi
 cmake .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-    $STABLEHLO_FLAG
+    $CUSTOM_PATHS
 
 echo "Compiling shared library..."
 if [[ "$OSTYPE" == "darwin"* ]]; then
